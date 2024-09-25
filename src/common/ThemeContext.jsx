2a05ae08434fe1c4575f05adc5795998a559fd82
Vijayraj Tolnoorkar <kumaarnt@gmail.com>
2024-09-25 10:51:03 -0700

    implemented toggle theme functions for dark mode feature

--- src/common/ThemeContext.jsx
+++ src/common/ThemeContext.jsx
@@ -1,9 +1,29 @@
 import React, { createContext, useContext, useEffect, useState} from 'react';
 
-const ThemeContext = createContxt();
+const ThemeContext = createContext();
 
 export const useTheme = () => useContext({children}) => {
     const [theme, seTheme] = useState(
         () => localStorage.getItem('theme')  || 'light'
     );
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+    };
+
+
+    return(
+        // <ThemeContext.Provider>  </ThemeContext.Provider>
+
+        <ThemeContext.Provider value= {{teme, toggleTheme}}> {children} </ThemeContext.Provider>
+        
+
+    )
+
+    
 };
