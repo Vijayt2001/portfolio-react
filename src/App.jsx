df26949e901e054aca1748eeba9681fc14c2ca8c
Vijayraj Tolnoorkar <kumaarnt@gmail.com>
2024-09-29 00:21:11 -0700

    added projects section to list the projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
 function App() {
   console.log('test')
  
   return ( <>
   
-  <Hero />;
+  <Hero />
+  <Projects />
+
 
 </>
   );
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './ProjectStyles.module.css'
+
+function Projects() {
+  return (
+    <section id='projects' className={StyleSheet.container}>
+        <h1 className='sectionTitle'>Projects</h1>
+        <div className= {styles.projectsContainer}>
+            <a href="https://github.com/Vijayt2001/2D-Tank-War-Game"></a>
+        </div>
+
+    </section>
+
+  )
+}
+
+export default Projects
