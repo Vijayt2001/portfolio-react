0e816f9c521d509f40fb33b68c678c93e0e9d8d8
Vijayraj Tolnoorkar <kumaarnt@gmail.com>
2024-09-30 14:40:31 -0700

    implemented css for projects module

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -4,7 +4,7 @@ import viberr from '../../assets/viberr.png'
 
 function Projects() {
   return (
-    <section id='projects' className={StyleSheet.container}>
+    <section id='projects' className={styles.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className= {styles.projectsContainer}>
             <a href="https://github.com/Vijayt2001/2D-Tank-War-Game">
