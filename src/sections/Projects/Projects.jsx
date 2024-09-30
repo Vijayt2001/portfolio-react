a997c7ae19e8271c98aa9d46be786b89bad26182
Vijayraj Tolnoorkar <kumaarnt@gmail.com>
2024-09-30 13:34:08 -0700

    implemnted classe names and Id's for project section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import styles from './ProjectStyles.module.css'
+import viberr from '../../assets/viberr.png'
 
 function Projects() {
   return (
     <section id='projects' className={StyleSheet.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className= {styles.projectsContainer}>
-            <a href="https://github.com/Vijayt2001/2D-Tank-War-Game"></a>
+            <a href="https://github.com/Vijayt2001/2D-Tank-War-Game">
+            <img className='hover' src={viberr} alt='project logo'/>
+            <h3>SFSU Social Media</h3>
+            <p>Social Media Website</p>
+            </a>
         </div>
-
     </section>
 
   )
