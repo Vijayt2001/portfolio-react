dfb4e16c4b86a49c26058b1bdf367ee339b2dff2
Vijayraj Tolnoorkar <kumaarnt@gmail.com>
2024-10-02 15:07:09 -0700

    added name, email and message form fo contact section

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -11,8 +11,43 @@ function Contact() {
           <label htmlFor="name" hidden>
             Name
           </label>
-          <input type="text" name="name" id="name" placeholder="Name" required/>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
         </div>
+
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+
+        <div className="formGroup">
+          <label htmlFor="message" hidden>
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            required ></textarea>
+          
+        </div>
+        <input className="hover btn" type="submit" 
+        value="submit"
+        />
+        
       </form>
     </section>
   );
