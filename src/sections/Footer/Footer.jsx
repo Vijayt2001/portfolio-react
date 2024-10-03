a24536ad863c7cbcbd840a9e85c93fa53afcb22d
Vijayraj Tolnoorkar <kumaarnt@gmail.com>
2024-10-03 14:57:22 -0700

    added footer

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './FooterStyles.module.css'
+
+function Footer() {
+  return (
+
+    <section id='footer' className={styles.container}>
+
+        <p>©</p>
+
+    </section>
+  )
+}
+
+export default Footer
